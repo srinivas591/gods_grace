52d65817bafa72704c68538d9e5de29b67a30e84
srinivas591 <76437251+srinivas591@users.noreply.github.com>
2021-05-07 02:11:47 +0530

    Add files via upload

---
+++ frames.css
@@ -0,0 +1,7 @@
+var autoPlayVideo = document.getElementById("ocScreencapVideo");
+    autoPlayVideo.oncanplaythrough = function() {
+        autoPlayVideo.muted = true;
+        autoPlayVideo.play();
+        autoPlayVideo.pause();
+        autoPlayVideo.play();
+   
